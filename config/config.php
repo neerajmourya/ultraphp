ba8583dbd326d4301a48c15883d32ebe89cb8eff
Neeraj Mourya <neerajmorya@gmail.com>
2017-07-29 15:04:13 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,14 +1,19 @@
 <?php
 ob_start();
 session_start();
+
 define('BASE_DIR', dirname(__DIR__));
 define('APP_DIR', BASE_DIR.'/app/');
-
-//Enter the base url
 define('BASE_URL', 'http://www.egrapes.in/');
-
-//Enter the sub folder if installed in subfolder i.e.,http://www.egrapes.in/ultraphp/
 //define('SUB_FOLDER', '/ultraphp/');
+
+//Inclues URL
+define('PLUGINS_DIR', APP_DIR.'plugins/');
+
+//Assets URL
+define('ASSETS_URL', BASE_URL.'app/assets/');
+define('ASSETS_ADMIN_URL', ASSETS_URL.'admin/');
+define('ASSETS_FRONTEND_URL', ASSETS_URL.'frontend/');
 
 //Enable Default routing
 define('DEFAULT_ROUTES',TRUE);
