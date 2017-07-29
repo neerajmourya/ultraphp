66417259c0a50028db60bc3c9ad2f6498e6b5bd9
Neeraj Mourya <neerajmorya@gmail.com>
2017-07-29 15:08:57 +0530

    Update UltraController.php

--- core/UltraController.php
+++ core/UltraController.php
@@ -1,10 +1,25 @@
 <?php
 class UltraController{
+    var $errors;
     public function __construct() {
-        
+        $this->errors = array();
     }
     
     public function execute(){
         
     }
+    
+    public function handleRequest(){
+        if($_SERVER['REQUEST_METHOD']==='POST'){
+            $this->submit(); 
+            return true;
+        }   
+        return false;
+    }
+    
+    public function submit(){
+        
+    }
+    
+    
 }
